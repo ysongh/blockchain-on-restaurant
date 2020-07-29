fc583377d20a705ed3ec8a05644504573112ab24
You Song Hou <yousonghou@yahoo.com>
2020-07-28 23:31:15 -0400

    add more fields for restaurant schema

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -3,9 +3,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const RestaurantSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter the name of the restaurant"]
+    },
+    price: {
+        type: Number,
+        required: [true, "Please enter the price of the deal"]
+    },
+    location: {
+        type: String,
+        required: [true, "Please enter the location of the restaurant"]
+    },
+    description: {
+        type: String
+    },
     image: {
         type: String
     },
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = mongoose.model('Restaurant', RestaurantSchema);
