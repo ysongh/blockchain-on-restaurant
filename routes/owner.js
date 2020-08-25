e30cd25e39dbc1497054b33bab2fd6a3e1245717
You Song Hou <yousonghou@yahoo.com>
2020-08-25 00:45:37 -0400

    add a route to login the owner

--- routes/owner.js
+++ routes/owner.js
@@ -21,10 +21,36 @@ router.post('/register', async (req, res) => {
 
         const dataOwner = await Owner.create(newOwner);
 
-        return res.status(200).json({ data: dataOwner });
+        return res.status(201).json({ data: dataOwner });
     } catch(err){
         console.error(err);
     }
 });
+
+// PUT /api/owner/login
+// login owner
+router.put('/login', async (req, res) => {
+    try{
+        const owner = await Owner.findOne({ email: req.body.email });
+
+        if(!owner){
+            return res.status(400).json({ errors: 'Email not found' });
+        }
+
+        if(req.body.password != owner.password){
+            return res.status(400).json({ errors: 'Invalid email or password' });
+        }
+
+        const loginData = {
+            name: owner.name,
+            email: owner.email,
+        };
+
+        return res.status(200).json({ data: loginData });
+    } catch(err){
+        console.error(err);
+    }
+});
+module.exports = router;
 
 module.exports = router;
